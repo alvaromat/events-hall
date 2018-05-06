227eff5d4e6a059e767532c73ea904b8c57dc010
Álvaro Mateos <alvaro_mat@yahoo.es>
2018-05-06 21:53:14 +0200

    Added ElectronService provider to the sandbox

--- src/app/components/presentation-list/presentation-list.component.sandbox.ts
+++ src/app/components/presentation-list/presentation-list.component.sandbox.ts
@@ -2,10 +2,11 @@ import { sandboxOf } from 'angular-playground';
 import { PresentationListComponent } from './presentation-list.component';
 import { MaterialComponentsModule } from '../../material-components.module';
 import { PresentationService } from '../../providers/presentation.service';
+import { ElectronService } from '../../providers/electron.service';
 
 export default sandboxOf(PresentationListComponent, {
   imports: [MaterialComponentsModule],
-  providers: [PresentationService]
+  providers: [PresentationService, ElectronService]
 })
 .add('Presentation-list alone',  {
   template: '<app-presentation-list></app-presentation-list>'
