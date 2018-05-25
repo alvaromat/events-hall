5c82034787a5f0897efd25feaa0c040431adc0bd
Álvaro Mateos <alvaro_mat@yahoo.es>
2018-05-25 16:38:18 +0200

    Removed order option

--- src/app/dynamic-forms/question-base.ts
+++ src/app/dynamic-forms/question-base.ts
@@ -11,14 +11,12 @@ export class QuestionBase<T> {
       key?: string,
       label?: string,
       required?: boolean,
-      order?: number,
       controlType?: string
     } = {}) {
     this.value = options.value;
     this.key = options.key || '';
     this.label = options.label || '';
     this.required = !!options.required;
-    this.order = options.order === undefined ? 1 : options.order;
     this.controlType = options.controlType || '';
   }
 }
