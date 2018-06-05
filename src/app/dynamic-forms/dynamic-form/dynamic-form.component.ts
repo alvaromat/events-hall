8027207b1b3fa119ea679833e9f2258cb5d91fb7
Álvaro Mateos <alvaro_mat@yahoo.es>
2018-06-05 16:12:26 +0200

    Changed button style and added an EventEmitter

--- src/app/dynamic-forms/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-forms/dynamic-form/dynamic-form.component.ts
@@ -1,20 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { QuestionBase } from '../question-base';
 import { QuestionControlService } from '../question-control.service';
 
+
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
-  providers: [ QuestionControlService ]
+  providers: [ QuestionControlService ],
+  styleUrls: ['./dynamic-form.component.scss']
 })
 export class DynamicFormComponent implements OnInit {
 
   @Input() questions: QuestionBase<any>[] = [];
   form: FormGroup;
-  payLoad = '';
-  @Input() showSubmit = false;
+  @Output() save = new EventEmitter<Object>();
 
   constructor(private qcs: QuestionControlService) {  }
 
@@ -23,6 +24,6 @@ export class DynamicFormComponent implements OnInit {
   }
 
   onSubmit() {
-    this.payLoad = JSON.stringify(this.form.value);
+    this.save.emit(this.form.value);
   }
 }
