301696e9487809559f482c7ac1af6c2fa4338459
Álvaro Mateos <alvaro_mat@yahoo.es>
2018-06-14 18:50:06 +0200

    Auto-save implemented

--- src/app/twitter/twitter-form/twitter-form.component.ts
+++ src/app/twitter/twitter-form/twitter-form.component.ts
@@ -1,20 +1,49 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild
+} from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { SearchOperatorsTableComponent } from '../search-operators-table/search-operators-table.component';
+import { FormControl, Validators, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-twitter-form',
   templateUrl: './twitter-form.component.html',
   styleUrls: ['./twitter-form.component.scss']
 })
-export class TwitterFormComponent {
-
+export class TwitterFormComponent implements OnInit {
   @Input() configuration: any;
+  @Output() save = new EventEmitter<null>();
+
+  @ViewChild('form') public form: NgForm;
+
+  itemsFormControl = new FormControl('', [Validators.required]);
+
+  refreshFormControl = new FormControl('', [
+    Validators.min(5),
+    Validators.required
+  ]);
 
   constructor(private dialog: MatDialog) {}
+
+  ngOnInit() {
+    this.form.valueChanges.pipe(
+      debounceTime(2000)
+    ).subscribe(_ => this.formChanged());
+  }
 
   showOperators() {
     this.dialog.open(SearchOperatorsTableComponent);
   }
 
+  formChanged() {
+    if (this.form.valid) {
+      this.save.emit();
+    }
+  }
 }
